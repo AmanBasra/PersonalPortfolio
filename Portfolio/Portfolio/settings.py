872721ec05d2e40dd25d1f5dcb609f4b836b64dc
A-Basra <129012687+AmanBasra@users.noreply.github.com>
2023-05-01 15:49:26 +0100

    Update

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j^b2#gu^bm94-5p7wj=_r43ff7$d2edq#7d2%tj*qo+#&1n=vt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['20.108.173.36', 'amanbasra.co.uk', 'localhost', '127.0.0.1']
 
@@ -117,8 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_ROOT=os.path.join(BASE_DIR, "Static")
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
